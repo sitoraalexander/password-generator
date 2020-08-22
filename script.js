58165b32f1b499452195c44b2c16275fe243b9cd
Sitora Alexander <sitoraalexander@gmail.com>
2020-08-22 17:48:59 -0400

    Complete JS, tested

--- script.js
+++ script.js
@@ -1,14 +1,236 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+]; 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Function to promt user for password options
+function getPasswordOptions() {
 
-  passwordText.value = password;
+  //Variable to store the length of password from user input
+  var length = parseInt(
+    prompt('How many characrters would you like in your password?')
+  );
+
+  //Conditional statement to check if the password length is a specified number. 
+  if (isNaN(length) === true) {
+    alert('Password length must be provided as a number');
+    return;
+  }
+
+  //Conditional statement to check if the password length is at least 8 characters long
+  if (length < 8) {
+    alert ('Password lengh must be at least 8 charachters');
+    return;
+  }
+
+  // Conditional statement to check if the password has less than 128 characters.
+  if (length > 128) {
+    alert('Password lengh must be less than 128 characters');
+    return;
+  }
+
+  // Variable to store boolean regarding the inclusion of special characters
+  var hasSpecialCharacters = confirm(
+    'Click OK to confirm including special characters.'
+  );
+
+  // Variable to store boolean regarding the inclusion of numeric characters
+  var hasNumericCharacters = confirm(
+    'Click OK to confirm including numeric characters.'
+  );  
+
+  // Variable to store boolean regarding the inclusion of lowercase characters
+  var hasLowerCasedCharacters = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );  
+
+  // Variable to store boolean regarding the inclusion of uppercase characters
+  var hasUpperCasedCharacters = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );  
+
+  // Conditional statement to check if user does not include any types of characters
+
+  if (
+    hasSpecialCharacters === false &&
+    hasNumericCharacters === false &&
+    hasLowerCasedCharacters === false &&
+    hasUpperCasedCharacters === false 
+  ){
+    alert('Must select at least one character type');
+    return;
+  }
+
+  // Object to store user input
+  var PasswordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters
+  };
+
+  return PasswordOptions;
 
 }
 
+// Function for getting a random element from an array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement
+}
+
+//Function to generate password with user input
+function generatePassword() {
+  var options = getPasswordOptions();
+  // Variable to store the password as it's being concatenated
+  var result = [];
+
+  // Array to store types of characters to include in password
+  var possibleCharacters = [];
+
+  // Array to contain one of each required character
+  var guaranteedCharacters = [];
+
+  // Conditional Statement that adds array of special characters into array of possible characters
+  // Get and add a random special character to guaranteedCharacters
+  if (options.hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+    }
+
+  // Conditional Statement that adds array of numeric characters into array of possible characters
+  // Get and add a random numeric character to guaranteedCharacters
+   if (options.hasNumericCharacters) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteedCharacters.push(getRandom(numericCharacters));
+    }
+
+  // Conditional Statement that adds array of lowercase characters into array of possible characters
+  // Get and add a random lowercase character to guaranteedCharacters
+   if (options.hasLowerCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+    }
+
+  // Conditional Statement that adds array of uppercase characters into array of possible characters
+  // Get and add a random uppercase character to guaranteedCharacters
+   if (options.hasUpperCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+    }
+  
+  // For loop to create password based on lengh requirement
+  for (var i = 0; i < options.length; i++) {
+    var possibleCharacter = getRandom(possibleCharacters);
+
+    result.push(possibleCharacter);
+    }
+
+  // Mix in at least one of each guaranteedCharacter in the result
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    result[i] = guaranteedCharacters [i];
+  }
+
+  // Transform the result into a string and pass into writePassword
+  return result.join('');
+}
+
+// Reference to #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
